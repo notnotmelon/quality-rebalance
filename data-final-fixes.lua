e1cea6bfe8e45b6b3afbb39f80305e583bbeb408
notnotmelon <zacharypicco@gmail.com>
2024-10-02 18:18:43 -0500

    feat: Add more entities to quality effected list

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,5 +1,45 @@
 local buffed_entities_to_extend = {}
 
+-- returns the number and the si unit
+local function big_number_format(n)
+	local si_unit = 0
+	while n >= 1000 do
+		n = n / 1000
+		si_unit = si_unit + 1
+	end
+	
+	return n, si_unit
+end
+
+local si_units = {
+	'si-prefix-symbol-kilo',
+	'si-prefix-symbol-mega',
+	'si-prefix-symbol-giga',
+	'si-prefix-symbol-tera',
+	'si-prefix-symbol-peta',
+	'si-prefix-symbol-exa',
+	'si-prefix-symbol-zetta',
+	'si-prefix-symbol-yotta',
+	'si-prefix-symbol-ronna',
+	'si-prefix-symbol-quetta',
+}
+
+-- formats a number with a certian number of decimal points and adds the si unit
+-- creates a localised string
+local function big_number_si_format(n, decimal_points)
+	local n, si_unit = big_number_format(n, decimal_points)
+	n = string.format('%.' .. (decimal_points or 1) .. 'f', n)
+	n = n:gsub('%.0+$', '')
+	if si_unit == 0 then return n end
+	return {'', n, {si_units[si_unit]}}
+end
+
+local function big_number_km_per_hr_format(n, decimal_points)
+	n = string.format('%.' .. (decimal_points or 1) .. 'f', n)
+	n = n:gsub('%.0+$', '')
+	return {'', n, {'si-unit-kilometer-per-hour'}}
+end
+
 -- returns the default buff amount per quality level in vanilla
 local function get_quality_buff(quality_level)
 	return 1 + quality_level * 0.3
@@ -55,7 +95,8 @@ local function add_quality_factoriopedia_info(entity, factoriopedia_info)
 			local quality_level = quality.level
 			if quality.hidden then goto continue end
 
-			local quality_buff = tostring(factoriopedia_function(entity, quality_level))
+			local quality_buff = factoriopedia_function(entity, quality_level)
+			if type(quality_buff) ~= 'table' then quality_buff = tostring(quality_buff) end
 			table.insert(localised_string, {'', '\n[img=quality.' .. quality.name .. '] ', {'quality-name.' .. quality.name}, ': [font=default-semibold]', quality_buff, '[/font]'})
 			::continue::
 		end
@@ -192,6 +233,86 @@ for _, agricultural_tower in pairs(data.raw['agricultural-tower']) do
 			local new_radius = (entity.radius + quality_level / 2) * growth_grid_tile_size
 			return new_radius - new_radius % growth_grid_tile_size
 		end}
+	})
+end
+
+for _, offshore_pump in pairs(data.raw['offshore-pump']) do
+	make_effected_by_quality(offshore_pump, function(entity, quality_level)
+		entity.pumping_speed = entity.pumping_speed * get_quality_buff(quality_level)
+	end, {
+		{'description.pumping-speed'}
+	}, {
+		{{'quality-tooltip.pumping-speed'}, function(entity, quality_level) return tostring(60 * entity.pumping_speed * get_quality_buff(quality_level)) .. '/s' end}
+	})
+end
+
+local function parse_energy(energy)
+	local energy_suffix = energy:match('[a-zA-Z]*$', 1)
+	local energy = tonumber(energy:match('[0-9]+', 1)) or 1
+	return energy, energy_suffix
+end
+
+for _, locomotive in pairs(data.raw.locomotive) do
+	make_effected_by_quality(locomotive, function(entity, quality_level)
+		local max_power, energy_suffix = parse_energy(entity.max_power)
+		entity.max_power = math.floor(max_power * get_quality_buff(quality_level)) .. energy_suffix
+		entity.max_speed = entity.max_speed * (1 + quality_level / 10)
+	end, {
+		{'description.acceleration-power'},
+		{'description.max-speed'}
+	}, {
+		{{'quality-tooltip.acceleration-power'}, function(entity, quality_level)
+			local max_power, energy_suffix = parse_energy(entity.max_power)
+			return math.floor(max_power * get_quality_buff(quality_level)) .. energy_suffix
+		end},
+		{{'quality-tooltip.max-speed'}, function(entity, quality_level) return big_number_km_per_hr_format(215.83 * entity.max_speed * (1 + quality_level / 20)) end}
+	})
+end
+
+for _, cargo_wagon in pairs(data.raw['cargo-wagon']) do
+	make_effected_by_quality(cargo_wagon, function(entity, quality_level)
+		entity.inventory_size = math.floor(entity.inventory_size * get_quality_buff(quality_level))
+	end, {
+		{'description.storage-size'}
+	}, {
+		{{'quality-tooltip.storage-size'}, function(entity, quality_level)
+			return math.floor(entity.inventory_size * get_quality_buff(quality_level))
+		end}
+	})
+end
+
+for _, fluid_wagon in pairs(data.raw['fluid-wagon']) do
+	make_effected_by_quality(fluid_wagon, function(entity, quality_level)
+		entity.capacity = math.floor(entity.capacity * (1 + quality_level / 5))
+	end, {
+		{'description.fluid-capacity'}
+	}, {
+		{{'quality-tooltip.storage-volume'}, function(entity, quality_level)
+			return big_number_si_format(entity.capacity * (1 + quality_level / 5))
+		end}
+	})
+end
+
+for _, lamp in pairs(data.raw.lamp) do
+	make_effected_by_quality(lamp, function(entity, quality_level)
+		entity.light = {intensity = entity.light.intensity, size = entity.light.size * get_quality_buff(quality_level)}
+	end, {
+		{'description.light-size'}
+	}, {
+		{{'quality-tooltip.light-size'}, function(entity, quality_level) return entity.light.size * get_quality_buff(quality_level) end}
+	})
+end
+
+for _, storage_tank in pairs(data.raw['storage-tank']) do
+	make_effected_by_quality(storage_tank, function(entity, quality_level)
+		entity.fluid_box.volume = entity.fluid_box.volume * (1 + quality_level / 5)
+	end, {
+		{'description.fluid-capacity'}
+	}, {
+		{{'quality-tooltip.storage-volume'}, function(entity, quality_level)
+			return big_number_si_format(entity.fluid_box.volume * (1 + quality_level / 5))
+		end
+		}
 	})
 end
 
