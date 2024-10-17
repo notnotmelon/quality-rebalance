75e18a603a2e7dc7f931086c664c645e9aefdf1d
notnotmelon <zacharypicco@gmail.com>
2024-10-17 10:35:50 -0500

    feat: Removed haste makes waste

--- data.lua
+++ data.lua
@@ -1,1 +1,9 @@
 require "prototypes.promethium-reprocessing"
+
+-- remove "haste makes waste"
+
+data.raw.module["speed-module"].effect.quality = nil
+for i = 2, 50 do
+    local module = data.raw.module["speed-module-" .. i]
+    if module then module.effect.quality = nil end
+end
